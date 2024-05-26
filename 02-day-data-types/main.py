e3d38d86c5e9334af7365f7b81db2cfd604f45fa
Denes-cilwal <dinesh.silwal@wesionary.team>
2024-05-26 20:15:30 +0545

    day-2 module

---
+++ 02-day-data-types/main.py
@@ -0,0 +1,45 @@
+# data types
+# string
+
+print("hello"[4])
+
+# type int has no len()
+# print(len(123))
+
+
+# concatenation
+print("hello" + " " + "world")
+
+# Float
+
+var = 3.1456
+
+# Boolean
+
+
+num_char = len(input("What is your name?\n"))
+print(num_char)
+# print("Your name has" + num_char + "characters.")
+
+# can only concatenate str (not "int") to str
+
+# type conversion int to num
+print(type(str(num_char)))
+
+
+print(70 + float(123))
+
+# assignment
+
+two_digit_number = input()
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+# add two digit together
+
+two_digit_number = first_digit + second_digit
+print(two_digit_number)
+
+
+
+
---
+++ 02-day-data-types/operators/main.py
@@ -0,0 +1,41 @@
+add = 3 + 5
+print(add)
+
+sub = 3-4
+print(sub)
+
+multiply = 3 * 4
+print(multiply)
+
+# bod mas
+result = 3 * 3 + 3 - 3
+print(result)
+
+
+# assignment
+
+# height = input()
+
+# weight = input()
+
+# bmi = weight / height * height
+# print(bmi)
+
+# round to 2 precision
+print(round(2.66666, 2))
+
+# dividend
+
+print(8 // 1)
+
+# user scores point
+score = 0
+score -= 1
+print(score)
+
+
+# f-string - https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
+# formatted string literals - python 3.6
+print(f"your score is {score}")
+
+
